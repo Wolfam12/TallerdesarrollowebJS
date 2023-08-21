26282333ed2ab4e434199380b154bde378a37069
Wolfam Diaz Galeano <wolfamc.diazg@konradlorenz.edu.co>
2023-08-20 20:27:37 -0500

    Adicion Tabla clientes
    Se adiciono tabla clientes

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,9 @@
 $("#nuevoUsuario").click(function() {
     var nombre = $("#nombre").val(); //capturar el valor del campo con id nombre
     var identificacion = $("#identificacion").val(); //capturar el valor del campo con id identificacion
-    $('#usuarios tr:last').after('<tr><td>' + nombre + '</td><td>' + identificacion + '</td></tr>'); //agrega un nuevo registro a la tabla, después del último tr
+    var PropietarioPlaca = $("#PropietarioPlaca").val(); //capturar el valor del campo con id identificacion
+    var IngresoVehiculo = $("#IngresoVehiculo").val(); //capturar el valor del campo con id identificacion
+    
+    $('#usuarios tr:last').after('<tr><td>' + nombre + '</td><td>' + identificacion + '</td></td> '+'</td><td>' + PropietarioPlaca + '</td></td>'+'</td><td>' + IngresoVehiculo + '</td></tr>'); //agrega un nuevo registro a la tabla, después del último tr
     $('#nuevoUsuarioModal').modal('toggle'); //cierra el modal
 });
